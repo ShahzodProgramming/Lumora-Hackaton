dbaf72a069f8769d7395885c6f49ce80a0bae745
ShahzodProgramming <bebap81246@gmail.com>
2025-09-28 14:39:05 +0500

    navbar $ bir nima

--- src/Approutes.jsx
+++ src/Approutes.jsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import NotFound from "./pages/NotFound";
 
 const Approutes = () => {
   return (
@@ -13,7 +14,7 @@ const Approutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign" element={<SignUpPage />} />
-        <Route path="*" element={<SignUpPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className='bg-red-'>
-        Sorry we couldn't found the page you were looking for
-    </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center mt-20 text-center px-4">
+      {/* Message Box */}
+      <div className="bg-red-500 text-white px-6 py-4 rounded-lg shadow-md mb-6 max-w-md">
+        <h1 className="text-3xl font-bold mb-2">404 - Page Not Found</h1>
+        <p className="text-lg">
+          Sorry, we couldn’t find the page you were looking for.
+        </p>
+      </div>
 
-export default NotFound
+      {/* Back Button */}
+      <Link to="/">
+        <button className="bg-[#8DD3BB] text-gray-900 font-semibold px-6 py-3 rounded-lg shadow-md transition duration-300 hover:bg-[#76cbb0]">
+          Go back to Home
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
