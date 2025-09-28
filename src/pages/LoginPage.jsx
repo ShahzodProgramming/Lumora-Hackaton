3f4eabaaf9bc830e209146f172f079f00a3e1f77
ShahzodProgramming <bebap81246@gmail.com>
2025-09-28 13:35:45 +0500

    Sign up page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const LoginPage = () => {
-  const [username, setUser]
 
 
   useEffect(() => {
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const SignUpPage = () => {
+  return (
+    <div className="big-ass-wrapper w-[100%] h-screen flex justify-center items-center">
+      <div className="flex flex-col w-[90%] mx-auto max-w-xl shadow-xl rounded-xl p-10 bg-gray-50 border-3 border-greenish ">
+        <h1 className="text-center text-4xl font-bold">Sign up</h1>
+        <form className="flex flex-col gap-10 mt-15 items-center w-full">
+          <div className="form-wrapper flex flex-col w-full transition-all">
+            <label>Username</label>
+            <input
+              className="border-3 rounded outline-0 py-2 px-5 border-gray-300 transition-all focus:border-greenish focus:bg-white"
+              type="text"
+              name="Username"
+              placeholder="Enter your username"
+            />
+          </div>
+
+          <div className="form-wrapper flex flex-col w-full transition-all">
+            <label>Email</label>
+            <input
+              className="border-3 rounded outline-0 py-2 px-5 border-gray-300 transition-all focus:border-greenish focus:bg-white"
+              type="email"
+              name="Password"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div className="form-wrapper flex flex-col w-full transition-all">
+            <label>Password</label>
+            <input
+              className="border-3 rounded outline-0 py-2 px-5 border-gray-300 transition-all focus:border-greenish focus:bg-white"
+              type="password"
+              name="Password"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <button className="bg-[#8DD3BB] p-3 rounded w-30 hover:shadow-xl hover:bg-[#7bb7a2] hover:text-white transition">
+            Confirm
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
